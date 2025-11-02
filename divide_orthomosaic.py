87ae0441b1cf94c6b5e64fda41cdd1ceea8084a3
Aline <malta.a.aline@gmail.com>
2025-11-02 17:49:54 -0300

    fix: update crop_image() to save images in RGB rather than BGR

--- divide_orthomosaic.py
+++ divide_orthomosaic.py
@@ -6,10 +6,6 @@ import rasterio
 from rasterio.windows import Window
 
 
-def to_opencv_bgr(array):
-    return cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
-
-
 def generate_windows(width, height, size):
     for y in range(0, height, size):
         for x in range(0, width, size):
@@ -22,9 +18,8 @@ def crop_image(src, output_dir, size=512):
     for count, (_, _, window) in enumerate(generate_windows(src.width, src.height, size)):
         crop = src.read(window=window)  
         img = crop.transpose(1, 2, 0)   
-        bgr = to_opencv_bgr(img)
         path = os.path.join(output_dir, f"crop_{count:04}.png")
-        cv2.imwrite(path, bgr)
+        cv2.imwrite(path,img)
 
 
 def main():
