5e07e7a0035bf9ffd312856639705ad0491d4764
Aline <malta.a.aline@gmail.com>
2025-11-03 00:54:51 -0300

    refactor(data): remove data augmentation from DataGen

--- data/data_generator.py
+++ data/data_generator.py
@@ -5,13 +5,12 @@ import random
 from tensorflow.keras.utils import Sequence
 
 class DataGen(Sequence):
-    def __init__(self, rgb_dir, mask_dir, batch=4, shuffle=True, augment=True, **kwargs):
+    def __init__(self, rgb_dir, mask_dir, batch=4, shuffle=True, **kwargs):
         super().__init__(**kwargs)
         self.x = sorted(os.path.join(rgb_dir, f) for f in os.listdir(rgb_dir))
         self.y = sorted(os.path.join(mask_dir, f) for f in os.listdir(mask_dir))
         self.batch = batch
         self.shuffle = shuffle
-        self.augment = augment
         self.indices = list(range(len(self.x)))
         if shuffle:
             random.shuffle(self.indices)
@@ -25,8 +24,6 @@ class DataGen(Sequence):
             idx = self.indices[i * self.batch + j]
             img = cv2.imread(self.x[idx]) / 255.0
             mask = cv2.imread(self.y[idx], cv2.IMREAD_GRAYSCALE) / 255.0
-            if self.augment:
-                img, mask = self._augment(img, mask)
             xb.append(img)
             yb.append(mask[..., None])
         return np.array(xb), np.array(yb)
@@ -34,19 +31,4 @@ class DataGen(Sequence):
     def on_epoch_end(self):
         if self.shuffle:
             random.shuffle(self.indices)
-
-    def _augment(self, img, mask):
-        if random.random() < 0.5:
-            img = cv2.flip(img, 1)
-            mask = cv2.flip(mask, 1)
-        if random.random() < 0.5:
-            angle = random.uniform(-15, 15)
-            h, w = img.shape[:2]
-            M = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
-            img = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
-            mask = cv2.warpAffine(mask, M, (w, h), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_REFLECT)
-        if random.random() < 0.5:
-            img *= random.uniform(0.7, 1.3)
-            img = np.clip(img, 0, 1)
-        return img, mask
 
