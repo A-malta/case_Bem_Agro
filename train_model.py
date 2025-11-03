2462ded0047aa69e499d1534174549028633f5ec
Aline <malta.a.aline@gmail.com>
2025-11-02 23:02:00 -0300

    feat(cli): add CLI entry point for training UNet model

---
+++ train_model.py
@@ -0,0 +1,11 @@
+import argparse
+from training.train import train
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rgb", required=True)
+    parser.add_argument("--groundtruth", required=True)
+    parser.add_argument("--modelpath", required=True)
+    args = parser.parse_args()
+
+    train(args.rgb, args.groundtruth, args.modelpath)
